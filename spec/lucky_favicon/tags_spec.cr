fe921bf7ea3ac0e90a6a7b34d6f88be224218790
Wout <hi@wout.codes>
2025-10-02 13:04:44 +0200

    refactor: use new favicon structure from the real favicon generator

--- spec/lucky_favicon/tags_spec.cr
+++ spec/lucky_favicon/tags_spec.cr
@@ -14,37 +14,25 @@ describe LuckyFavicon::Tags do
   context "with defaults" do
     describe "#favicon_tags" do
       it "renders all tags" do
-        html = view(&.favicon_tags("Lucky App", "#00ff99"))
+        html = view(&.favicon_tags("Lucky App"))
 
         html.should contain <<-HTML
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=0.1.0">
+        <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png?v=0.1.0">
         HTML
         html.should contain <<-HTML
-        <link rel="mask-icon" color="#00ff99" href="/safari-pinned-tab.svg?v=0.1.0">
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg?v=0.1.0">
         HTML
         html.should contain <<-HTML
         <link rel="shortcut icon" href="/favicon.ico?v=0.1.0">
         HTML
         html.should contain <<-HTML
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=0.1.0">
-        HTML
-        html.should contain <<-HTML
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=0.1.0">
-        HTML
-        html.should contain <<-HTML
-        <link rel="manifest" href="/site.webmanifest?v=0.1.0">
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=0.1.0">
         HTML
         html.should contain <<-HTML
         <meta name="apple-mobile-web-app-title" content="Lucky App">
         HTML
         html.should contain <<-HTML
-        <meta name="application-name" content="Lucky App">
-        HTML
-        html.should contain <<-HTML
-        <meta name="msapplication-TileColor" content="#00ff99">
-        HTML
-        html.should contain <<-HTML
-        <meta name="theme-color" content="#00ff99">
+        <link rel="manifest" href="/site.webmanifest?v=0.1.0">
         HTML
       end
     end
@@ -59,37 +47,24 @@ describe LuckyFavicon::Tags do
       end
     end
 
-    describe "#safari_pinned_tab_tag" do
-      it "renders a safari mask icon with color" do
-        html = view(&.safari_pinned_tab_tag("#ff0066"))
-
-        html.should contain <<-HTML
-        <link rel="mask-icon" color="#ff0066" href="/safari-pinned-tab.svg?v=0.1.0">
-        HTML
-      end
-    end
-
     describe "#favicon_icon_tags" do
       it "renders tags with defaut sizes" do
         html = view(&.favicon_icon_tags)
 
         html.should contain <<-HTML
+        <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png?v=0.1.0">
+        HTML
+        html.should contain <<-HTML
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg?v=0.1.0">
+        HTML
+        html.should contain <<-HTML
         <link rel="shortcut icon" href="/favicon.ico?v=0.1.0">
-        HTML
-        html.should contain <<-HTML
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=0.1.0">
-        HTML
-        html.should contain <<-HTML
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=0.1.0">
         HTML
       end
 
       it "renders tags with custom sizes" do
         html = view(&.favicon_icon_tags(%w[24x24 64x64 128x128]))
 
-        html.should contain <<-HTML
-        <link rel="shortcut icon" href="/favicon.ico?v=0.1.0">
-        HTML
         html.should contain <<-HTML
         <link rel="icon" type="image/png" sizes="24x24" href="/favicon-24x24.png?v=0.1.0">
         HTML
@@ -98,6 +73,12 @@ describe LuckyFavicon::Tags do
         HTML
         html.should contain <<-HTML
         <link rel="icon" type="image/png" sizes="128x128" href="/favicon-128x128.png?v=0.1.0">
+        HTML
+        html.should contain <<-HTML
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg?v=0.1.0">
+        HTML
+        html.should contain <<-HTML
+        <link rel="shortcut icon" href="/favicon.ico?v=0.1.0">
         HTML
       end
     end
@@ -112,28 +93,12 @@ describe LuckyFavicon::Tags do
       end
     end
 
-    describe "#application_name_tags" do
-      it "renders the app name meta tags" do
-        html = view(&.application_name_tags("Lucky App"))
+    describe "#application_title_tag" do
+      it "renders the app name meta tag" do
+        html = view(&.application_title_tag("Lucky App"))
 
         html.should contain <<-HTML
         <meta name="apple-mobile-web-app-title" content="Lucky App">
-        HTML
-        html.should contain <<-HTML
-        <meta name="application-name" content="Lucky App">
-        HTML
-      end
-    end
-
-    describe "#theme_color_tags" do
-      it "renders the them color meta tags" do
-        html = view(&.theme_color_tags("#ff0066"))
-
-        html.should contain <<-HTML
-        <meta name="msapplication-TileColor" content="#ff0066">
-        HTML
-        html.should contain <<-HTML
-        <meta name="theme-color" content="#ff0066">
         HTML
       end
     end
@@ -142,37 +107,25 @@ describe LuckyFavicon::Tags do
   context "with dir and version" do
     describe "#favicon_tags" do
       it "renders all tags" do
-        html = view(&.favicon_tags("Lucky App", "#00ff99", "1.2.3", "/icons"))
+        html = view(&.favicon_tags("Lucky App", "1.2.3", "/icons"))
 
         html.should contain <<-HTML
-        <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png?v=1.2.3">
+        <link rel="icon" type="image/png" sizes="96x96" href="/icons/favicon-96x96.png?v=1.2.3">
         HTML
         html.should contain <<-HTML
-        <link rel="mask-icon" color="#00ff99" href="/icons/safari-pinned-tab.svg?v=1.2.3">
+        <link rel="icon" type="image/svg+xml" href="/icons/favicon.svg?v=1.2.3">
         HTML
         html.should contain <<-HTML
         <link rel="shortcut icon" href="/icons/favicon.ico?v=1.2.3">
         HTML
         html.should contain <<-HTML
-        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png?v=1.2.3">
-        HTML
-        html.should contain <<-HTML
-        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png?v=1.2.3">
-        HTML
-        html.should contain <<-HTML
-        <link rel="manifest" href="/icons/site.webmanifest?v=1.2.3">
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png?v=1.2.3">
         HTML
         html.should contain <<-HTML
         <meta name="apple-mobile-web-app-title" content="Lucky App">
         HTML
         html.should contain <<-HTML
-        <meta name="application-name" content="Lucky App">
-        HTML
-        html.should contain <<-HTML
-        <meta name="msapplication-TileColor" content="#00ff99">
-        HTML
-        html.should contain <<-HTML
-        <meta name="theme-color" content="#00ff99">
+        <link rel="manifest" href="/icons/site.webmanifest?v=1.2.3">
         HTML
       end
     end
@@ -187,28 +140,18 @@ describe LuckyFavicon::Tags do
       end
     end
 
-    describe "#safari_pinned_tab_tag" do
-      it "renders a safari mask icon with color" do
-        html = view(&.safari_pinned_tab_tag("#ff0066", "1.2.3", "/icons"))
-
-        html.should contain <<-HTML
-        <link rel="mask-icon" color="#ff0066" href="/icons/safari-pinned-tab.svg?v=1.2.3">
-        HTML
-      end
-    end
-
     describe "#favicon_icon_tags" do
       it "renders tags with defaut sizes" do
         html = view(&.favicon_icon_tags(version: "1.2.3", dir: "/icons"))
 
         html.should contain <<-HTML
+        <link rel="icon" type="image/png" sizes="96x96" href="/icons/favicon-96x96.png?v=1.2.3">
+        HTML
+        html.should contain <<-HTML
+        <link rel="icon" type="image/svg+xml" href="/icons/favicon.svg?v=1.2.3">
+        HTML
+        html.should contain <<-HTML
         <link rel="shortcut icon" href="/icons/favicon.ico?v=1.2.3">
-        HTML
-        html.should contain <<-HTML
-        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png?v=1.2.3">
-        HTML
-        html.should contain <<-HTML
-        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png?v=1.2.3">
         HTML
       end
     end
--- src/lucky_favicon/tags.cr
+++ src/lucky_favicon/tags.cr
@@ -1,19 +1,16 @@
 module LuckyFavicon::Tags
-  FAVICON_ICON_SIZES = %w[16x16 32x32]
+  FAVICON_ICON_SIZES = %w[96x96]
 
   # Generates a set of icon tags as suggested by https://realfavicongenerator.net/
   def favicon_tags(
     app_name : String,
-    theme_color : String,
     version : String? = nil,
-    dir : String? = nil
+    dir : String? = nil,
   ) : Nil
-    apple_touch_icon_tag(version, dir)
     favicon_icon_tags(FAVICON_ICON_SIZES, version, dir)
+    apple_touch_icon_tag(version, dir)
+    application_title_tag(app_name)
     site_webmanifest_tag(version, dir)
-    safari_pinned_tab_tag(theme_color, version, dir)
-    application_name_tags(app_name)
-    theme_color_tags(theme_color)
   end
 
   # Generates a apple-touch-icon link tag with a default size of 180x180:
@@ -26,47 +23,37 @@ module LuckyFavicon::Tags
   # This is also the default when generating the icon set.
   def apple_touch_icon_tag(
     version : String? = nil,
-    dir : String? = nil
+    dir : String? = nil,
   ) : Nil
     empty_tag "link", rel: "apple-touch-icon", sizes: "180x180",
       href: favicon_icon_path("apple-touch-icon.png", version, dir)
   end
 
-  # Generates a mask-icon link tag using the generated svg icon and the theme
-  # color for the backgound:
+  # Generates the default .ico shortcut icon link tag, an SVG icon tag, and
+  # also additional PNG icon(s), possibly in different sizes (currently only
+  # 96x96).
   #
   # ```html
-  # <link rel="mask-icon" href="/safari-pinned-tab.svg?v=0.1.0" color="#ff0066">
-  # ```
-  def safari_pinned_tab_tag(
-    theme_color : String,
-    version : String? = nil,
-    dir : String? = nil
-  ) : Nil
-    empty_tag "link", rel: "mask-icon", color: theme_color,
-      href: favicon_icon_path("safari-pinned-tab.svg", version, dir)
-  end
-
-  # Generates the default .ico shortcut icon link tag and also a list for png
-  # favicon icons with a number of given sizes (most likely 16x16 and 32x32):
-  #
-  # ```html
-  # <link rel="shortcut icon" href="/favicon.ico?v=0.1.0">
-  # <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=0.1.0">
-  # <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=0.1.0">
+  # <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+  # <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+  # <link rel="shortcut icon" href="/favicon.ico" />
   # ```
   def favicon_icon_tags(
     sizes : Array(String) = FAVICON_ICON_SIZES,
     version : String? = nil,
-    dir : String? = nil
+    dir : String? = nil,
   ) : Nil
-    empty_tag "link", rel: "shortcut icon",
-      href: favicon_icon_path("favicon.ico", version, dir)
-
     sizes.each do |size|
       empty_tag "link", rel: "icon", type: "image/png", sizes: size,
         href: favicon_icon_path("favicon-#{size}.png", version, dir)
     end
+
+    empty_tag "link", rel: "icon",
+      type: "image/svg+xml",
+      href: favicon_icon_path("favicon.svg", version, dir)
+
+    empty_tag "link", rel: "shortcut icon",
+      href: favicon_icon_path("favicon.ico", version, dir)
   end
 
   # Generates a manifest link tag for the sit's web manifest:
@@ -76,44 +63,28 @@ module LuckyFavicon::Tags
   # ```
   def site_webmanifest_tag(
     version : String? = nil,
-    dir : String? = nil
+    dir : String? = nil,
   ) : Nil
     empty_tag "link", rel: "manifest",
       href: favicon_icon_path("/site.webmanifest", version, dir)
   end
 
-  # Generates meta tags for the application name:
+  # Generates meta tag for the application title in iOS:
   #
   # ```html
   # <meta name="apple-mobile-web-app-title" content="Lucky App">
-  # <meta name="application-name" content="Lucky App">
   # ```
-  def application_name_tags(
-    app_name : String
+  def application_title_tag(
+    app_name : String,
   ) : Nil
     meta name: "apple-mobile-web-app-title", content: app_name
-    meta name: "application-name", content: app_name
-  end
-
-  # Generates meta tags for the global theme color, mostly used as a background
-  # to the base icon:
-  #
-  # ```html
-  # <meta name="msapplication-TileColor" content="#ff0066">
-  # <meta name="theme-color" content="#ff0066">
-  # ```
-  def theme_color_tags(
-    theme_color : String
-  )
-    meta name: "msapplication-TileColor", content: theme_color
-    meta name: "theme-color", content: theme_color
   end
 
   # Builds the path to the given file.
   def favicon_icon_path(
     icon : String,
     version : String? = nil,
-    dir : String? = nil
+    dir : String? = nil,
   )
     File.join(dir || "/", icon).rchop("/") + "?v=#{version || "0.1.0"}"
   end
